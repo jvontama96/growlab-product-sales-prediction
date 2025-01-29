a59220af48fd6cf8e16b6f9198334d196aaac578
jvontama96 <168260561+jvontama96@users.noreply.github.com>
2025-01-29 08:38:06 +0700

    Update app3.py

--- app3.py
+++ app3.py
@@ -43,11 +43,12 @@ tab1, tab2 = st.tabs(["Sales Prediction", "About"])
 
 # Sales Prediction Tab
 with tab1:
-    st.info(This app is designed to help Shopee online store user predict future sales based on key product attributes such as price, stock quantity, rating, location, and category.
+    st.info("""This app is designed to help Shopee online store user predict future sales based on key product attributes such as price, stock quantity, rating, location, and category.
     #### How It Works:
-        1. **Input Parameters**: Provide the required details about your product, including price, stock quantity, rating, location, and category.
-        2. **Predict Sales**: Click the **Predict Sales** button to get the predicted sales quantity and value for the next 30 days.)
-
+    1. **Input Parameters**: Provide the required details about your product, including price, stock quantity, rating, location, and category.
+    2. **Predict Sales**: Click the **Predict Sales** button to get the predicted sales quantity and value for the next 30 days.
+    """)
+    
     # Input fields in the main layout
     st.header("Input Parameters")
     col1, col2 = st.columns(2)
